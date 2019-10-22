073b31303f3bd56589b1e78f5293b23da79ab9f2
khalig-n <khalig.novruzli@gmail.com>
2019-10-22 15:51:46 +0300

    Added some styling

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var seedDB = require("./seeds");
 
 var app = express();
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {
