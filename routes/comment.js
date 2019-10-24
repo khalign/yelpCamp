fb285b84dcc88f3f5f63552e05feaa1044f3c6d5
khalig-n <khalig.novruzli@gmail.com>
2019-10-24 15:30:35 +0300

    Comments associated with user

--- routes/comment.js
+++ routes/comment.js
@@ -19,9 +19,9 @@ router.post("/", isAuth, function(req, res) {
       Comment.create(req.body.comment, function(err, comment) {
         if (err) console.log(err);
         else {
-          (comment.author.id = req.user._id),
-            (comment.author.username = req.user.username),
-            comment.save();
+          comment.author.id = req.user._id;
+          comment.author.username = req.user.username;
+          comment.save();
           campground.comments.push(comment);
           campground.save();
           res.redirect("/campgrounds/" + campground._id);
